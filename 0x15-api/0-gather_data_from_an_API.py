ee360552500489b317f4613ee0ee4f26f0b7f52e
Fattyk <ogundele.fatai.k@gmail.com>
2022-11-14 23:32:56 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ def progress(id):
     user = requests.get(url + "users/{}".format(id)).json()
     todos = requests.get(url + "todos", params={"userId": id}).json()
 
-    completed = [todo.get("completed") for todo in todos if todo.get("completed")  is True]
+    completed = [todo.get("title") for todo in todos if todo.get("completed")  is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
